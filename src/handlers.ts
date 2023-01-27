b9bd2c37df32007caebc05b10bd99d25940fcfaa
Norwod <cartoonrussian@mail.ru>
2023-01-27 16:01:12 +0600

    Update handlers.ts

--- src/handlers.ts
+++ src/handlers.ts
@@ -7,7 +7,53 @@ import {createOrderBundle} from './card'
 export const runAppHandler: SaluteHandler = ({ res, req }) => {
     const keyset = req.i18n(dictionary)
     res.appendBubble(keyset('start'))
-    res.appendCard(createOrderBundle());
+    res.appendCard({
+        type: 'list_card',
+        paddings: {
+            top: '9x',
+            bottom: '12x',
+            left: '8x',
+            right: '8x',
+        },
+        cells: [
+            {
+                type: 'text_cell_view',
+                content: {
+                    text: 'Корзина',
+                    typeface: 'headline3',
+                    text_color: 'default',
+                },
+                paddings: {
+                    bottom: '2x',
+                },
+            },
+            {
+                type: 'left_right_cell_view',
+                paddings: {
+                    top: '10x',
+                },
+                left: {
+                    type: 'simple_left_view',
+                    texts: {
+                        title: {
+                            text: 'Итого',
+                            typeface: 'footnote1',
+                            text_color: 'secondary',
+                        },
+                    },
+                },
+                right: {
+                    type: 'detail_right_view',
+                    detail: {
+                        text: `Text`,
+                        typeface: 'footnote2',
+                        text_color: 'default',
+                        max_lines: 0,
+                    },
+                },
+            },
+        ],
+    });
     res.appendSuggestions(['Рецепт', 'Хватит'])
 }
 
